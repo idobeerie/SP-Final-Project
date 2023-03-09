1c9dc56c5d67d21919b20ddaaa022831741dbae3
Ido Beerie <idobeerie@mail.tau.ac.il>
2023-03-09 12:53:58 +0200

    start

--- test.c
+++ test.c
@@ -46,3 +46,76 @@ double** create_laplacian_matrix(double** degree_matrix, double** adj_matrix, in
     }
     return laplacian_matrix;
 }
+
+void jacobi_rotation(double** A, int n, double** V) {
+    int i, j, k;
+    double p, y, t, c, s;
+    double* b = (double*) malloc(n * sizeof(double));
+    double* z = (double*) malloc(n * sizeof(double));
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            V[i][j] = (i == j) ? 1.0 : 0.0;
+        }
+    }
+    
+    while (1) {
+        p = 0.0;
+        for (i = 0; i < n-1; i++) {
+            for (j = i+1; j < n; j++) {
+                p += fabs(A[i][j]);
+            }
+        }
+        if (p < TOLERANCE) {
+            break;
+        }
+        
+        for (i = 0; i < n-1; i++) {
+            for (j = i+1; j < n; j++) {
+                if (fabs(A[i][j]) > TOLERANCE) {
+                    t = (A[j][j] - A[i][i]) / (2.0 * A[i][j]);
+                    if (t >= 0.0) {
+                        y = 1.0 / (t + sqrt(1.0 + t*t));
+                    } else {
+                        y = -1.0 / (-t + sqrt(1.0 + t*t));
+                    }
+                    c = 1.0 / sqrt(1.0 + y*y);
+                    s = y * c;
+                    for (k = 0; k < n; k++) {
+                        b[k] = c * A[i][k] - s * A[j][k];
+                        A[j][k] = s * A[i][k] + c * A[j][k];
+                    }
+                    for (k = 0; k < n; k++) {
+                        A[i][k] = b[k];
+                    }
+                    for (k = 0; k < n; k++) {
+                        b[k] = c * A[k][i] - s * A[k][j];
+                        A[k][j] = s * A[k][i] + c * A[k][j];
+                    }
+                    for (k = 0; k < n; k++) {
+                        A[k][i] = b[k];
+                    }
+                    for (k = 0; k < n; k++) {
+                        b[k] = c * V[k][i] - s * V[k][j];
+                        V[k][j] = s * V[k][i] + c * V[k][j];
+                    }
+                    for (k = 0; k < n; k++) {
+                        V[k][i] = b[k];
+                    }
+                }
+            }
+        }
+    }
+    free(b);
+    free(z);
+}
+    
+double** create_eigenvectors(double** laplacian_matrix, int n) {
+    double** eigenvectors = (double*) malloc(n * sizeof(double));
+    for (int i = 0; i < n; i++) {
+        eigenvectors[i] = (double*) malloc(n * sizeof(double));
+    }
+    jacobi_rotation(laplacian_matrix, n, eigenvectors);
+    return eigenvectors;
+}
+
+
