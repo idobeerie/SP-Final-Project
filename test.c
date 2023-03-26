61636b4e96bfc19b98eb2835d48b1e37079c54cd
Achinoam Klein <achinoamk@mail.tau.ac.il>
2023-03-26 10:54:54 +0300

    sorting a matrix
    lalala

--- test.c
+++ test.c
@@ -160,19 +160,47 @@ double cmpfunc (const void * a, const void * b) {
 }
 
 int find_number_k(int n + 1, double ** res_d){   // find the number of clusters
-    double k;    
+    double k;   
+    double* indices = malloc(n * sizeof(double)); 
+    double** temp_matrix = malloc(n * sizeof(double*));
     k = 0.0;   //maybe nir is right and this is not allowed
-    double* eigenVals = (double*) malloc(n * sizeof(double));
+    double* eigenVals = malloc(n * sizeof(double));
     for(int i = 0; i < n; i++){
         eigenVals[i] = res_d[0][i];
+        indices[i] = i;
     }
-    qsort(arr, n, sizeof(double), cmpfunc);
+    qsort(eigenVals, n, sizeof(double), cmpfunc);
+    
     for(int i = 1; i <= n / 2; i++){
         if(fabs(eigenVals[i] - eigenVals[i - 1]) > k){
             k = fabs(eigenVals[i] - eigenVals[i - 1]);
         }
     }
+
+    for (int i = 0; i < n; i++) {
+        int index = -1;
+        for (int j = 0; j < n; j++) {
+            if (res_d[0][j] == eigenvalues[i]) {
+                index = j;
+                break;
+            }
+        }
+        temp_matrix[i] = malloc(n * sizeof(double));
+
+        for (int j = 1; j < n+1; j++) {
+            //make this switch the columns
+        }
+
+        // copy the sorted matrix to the original matrix
+        for (int i = 0; i < n+1; i++) {
+            res_d[0][i] = eigenvalues[i];
+            for (int j = 0; j < n+1; j++) {
+                res_d[j][i] = temp_matrix[j][i];
+            }
+        }
+    }
     // free eigenVals
+    //return res_d maybe and do another function for the k
     return (int)k;
 }
 
