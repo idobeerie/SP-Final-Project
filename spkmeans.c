81c086bd3602423e451d625bd74359aa86e33e2b
Achinoam Klein <achinoamk@mail.tau.ac.il>
2023-03-30 17:48:31 +0300

    set zero
    i did change the the set zero function to get rows and cols

--- spkmeans.c
+++ spkmeans.c
@@ -18,7 +18,7 @@ double** allocateMatrix(size_t rows, size_t cols) {   // i really dont remember 
             matrix[i] = data+i*cols;
         }
     }
-    set_matrix_to_zero(matrix, rows);
+    set_matrix_to_zero(matrix, rows, cols);
 	return matrix;
 }
 
@@ -42,9 +42,9 @@ double** wam(double** centroids, int n, int d) {
     return adj_matrix;
 }
 
-void set_matrix_to_zero(double** matrix, int n) {    //helper function
+void set_matrix_to_zero(double** matrix, int n, int m) {    //helper function
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < m; j++) {
             matrix[i][j] = 0;
         }
     }
@@ -54,7 +54,7 @@ double** ddg(double** adj_matrix, int n) {
     double** degree_matrix = allocateMatrix(n, n);
     double degree = 0;
     // print_matrix(adj_matrix, n, n);
-    set_matrix_to_zero(degree_matrix, n);  
+    // set_matrix_to_zero(degree_matrix, n, n);  
     for (int i = 0; i < n; i++) {
         degree = 0;
         for (int j = 0; j < n; j++) {
